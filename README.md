1c33bb7202020dc251537b0b3566d0d7e9e024f0
tekendra-timsina <tekendra.timsina@gmail.com>
2020-02-15 16:19:02 +0200

    test updated

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # TDD_DJANGO
 This repository contains the code of project work based on the book " The Basics of TDD and Django" which can be obtained from the following link.
-[The Basics of TDD and Django](https://www.obeythetestinggoat.com/book/)
+[The Basics of TDD and Django](https://www.obeythetestinggoat.com/)
--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,34 @@
 # Importing webdriver to load firefox
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-assert 'Django' in browser.title
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # My wife has heard about a cool online to-do app. She goes to check out its homepage
+        self.browser.get('http://localhost:8000')
+
+        # She notied the  page title and header mentioned to-do lists 
+        self.assertIn('TO-DO', self.browser.title)
+        self.fail('Finish the test!')
+
+        # She is invited to enter a to-do item straight away 
+
+        # she types " Buy peacock feathers" into a text box
+
+        # when she hits enter, the page updates, and now the pages lists 
+        # 1. "Buy peacock feathers" as an item in a to-do list 
+        # 
+        # There is still a text box inviting her to add another item. when she enters another item the process repeats.
+        # 
+        # The site has generated a unique URL for her  and when she visits that URL , she noticed her to-do list are still there.
+
+        # satisfied, she goes back to sleep
+         
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
